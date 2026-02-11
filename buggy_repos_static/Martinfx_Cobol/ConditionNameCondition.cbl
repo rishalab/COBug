483cb770a39bd7c61b17ef8cda9a0987ee040bd6
rishalab <84390228+rishalab@users.noreply.github.com>
2026-02-11 17:15:41 +0530

    Add files via upload

---
+++ buggy_repos_static/Martinfx_Cobol/ConditionNameCondition.cbl
@@ -0,0 +1,43 @@
+      ******************************************************************
+      * Author: Maxfx
+      * Date: 12/5/2016
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. CONDITION-STATEMENT.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+
+       01  WS-FLAG PIC X VALUE 'N'.
+
+      ******************************************************************
+      * Set values for M_TRUE in interval <30, 100>
+      * Set values for M_FALSE in interval <000, 40>
+      ******************************************************************
+           01 M_NUMBER PIC 9(3).
+           88 M_TRUE  VALUES ARE 30 THRU 100.
+           88 M_FALSE VALUES ARE 000 THRU 40.
+
+       PROCEDURE DIVISION.
+       GO TO ERR-HANDLER.
+
+       DISPLAY "NORMAL-FLOW".
+
+       ERR-HANDLER.
+           DISPLAY "ERROR-HANDLED".
+           EXIT.
+
+       IF WS-FLAG = 'Y'
+           DISPLAY "FLAG-SET".
+       END-IF.
+
+       MAIN-PROCEDURE.
+           MOVE 50 TO M_NUMBER.
+
+           IF M_TRUE
+           DISPLAY 'Passed with ' M_NUMBER ' marks'.
+
+           IF M_FALSE
+           DISPLAY 'FAILED with ' M_NUMBER ' marks'.
+
+           GOBACK.
