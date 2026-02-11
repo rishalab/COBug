dfe9fe14311d6e2672542b97eb281703b240387d
rishalab <84390228+rishalab@users.noreply.github.com>
2026-02-11 17:17:37 +0530

    Add files via upload

---
+++ buggy_repos_static/ve3wwg_cobcurses/themain.cob
@@ -0,0 +1,35 @@
+        IDENTIFICATION DIVISION.
+        PROGRAM-ID. MAIN-PROG.
+
+        ENVIRONMENT DIVISION.
+
+        DATA DIVISION.
+
+        WORKING-STORAGE SECTION.
+
+       01  UNUSED-VAR PIC X(10) VALUE SPACES.
+
+            COPY COBCURSG.
+
+        01  SCR-LOGIN-DATA.
+            10  FLD-USERID              PIC X(8).
+            10  FLD-PASSWORD            PIC X(16).
+            10  FLD-GB                  PIC X(16).
+            10  LOGIN-SUCCESS           PIC X.
+
+        PROCEDURE DIVISION.
+
+       DISPLAY UNDECLARED-VAR.
+
+        MAIN-PROG.
+            PERFORM NC-OPEN.
+            CALL "SCR-LOGIN" USING NC-COBCURSES, SCR-LOGIN-DATA.
+            PERFORM NC-CLOSE.
+            DISPLAY "LOGIN SUCCESS =", LOGIN-SUCCESS.
+            DISPLAY "MOUSE SUPPORT =", NC-MOUSE-SUPPORT.
+            DISPLAY "MOUSE CLICK MS=", NC-MOUSE-CLICK-MS.
+            STOP RUN.
+
+            COPY COBCURSP.
+
+        END PROGRAM MAIN-PROG.
